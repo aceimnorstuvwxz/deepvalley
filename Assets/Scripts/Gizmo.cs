9fdb89b889ffa452ee9d70f082ac731699a19ec5
Chen Bingfeng <iichenbf@gmail.com>
2015-12-25 22:13:15 +0800

    back

---
+++ Assets/Scripts/Gizmo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+//Gizmos are used to give visual debugging or setup aids in the scene view.
+
+public class Gizmo : MonoBehaviour {
+
+	public float gizmoSize = .75f;
+	public Color gizmoColor = Color.yellow;
+
+	void Start()
+	{
+		Debug.Log ("hello world!");
+	}
+
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = gizmoColor;
+		Gizmos.DrawWireSphere (transform.position, gizmoSize);
+	}
+}
