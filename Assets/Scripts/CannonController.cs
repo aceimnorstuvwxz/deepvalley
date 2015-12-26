04ce7add3ac3f3a67fb88df720ffcbce80e9f0a7
Chen Bingfeng <iichenbf@gmail.com>
2015-12-26 22:49:16 +0800

    sdf

--- Assets/Scripts/CannonController.cs
+++ Assets/Scripts/CannonController.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class CannonController : MonoBehaviour {
 	public float shift_radio_threshold = 0.3f;
-	public float horizontal_rotate_speed = 1.0f;
-	public float vertical_rotate_speed = 1.0f;
+	public float horizontal_rotate_speed = 5.0f;
+	public float vertical_rotate_speed = 5.0f;
 	public float vertical_angle_max = 45.0f;
 	public float vertical_angle_min = -10.0f;
 	public float sight_scope_var = 90f;
@@ -12,6 +12,7 @@ public class CannonController : MonoBehaviour {
 	private GameObject cannonShifter;
 	private GameObject cannonBore;
 	private float currentVertalRotate;
+	private float moveRate;
 
 	private Camera mainCamera;
 
@@ -21,6 +22,7 @@ public class CannonController : MonoBehaviour {
 		cannonBore = GameObject.Find ("cannon-bore");
 		mainCamera = GameObject.Find ("main-camera").GetComponent<Camera> ();
 		currentVertalRotate = 0;
+		moveRate = 1f;
 	}
 	
 	// Update is called once per frame
@@ -37,14 +39,14 @@ public class CannonController : MonoBehaviour {
 		if (radioXY.magnitude < shift_radio_threshold) {
 			return;
 		}
-		cannonShifter.transform.Rotate (new Vector3(0,horizontal_rotate_speed*radioXY.x,0));
+		cannonShifter.transform.Rotate (new Vector3(0,horizontal_rotate_speed*radioXY.x*moveRate,0));
 
 		//!careful for euler stays in [0-360]
 		if (currentVertalRotate > vertical_angle_max && radioXY.y > 0 ||
 		    currentVertalRotate < vertical_angle_min && radioXY.y < 0) {
 			// forbid to rotate out of vertical field
 		} else {
-			float delta = vertical_rotate_speed*radioXY.y;
+			float delta = vertical_rotate_speed*radioXY.y*moveRate;
 			currentVertalRotate += delta;
 			cannonBore.transform.Rotate(new Vector3(0,0,-delta));
 		}
@@ -52,6 +54,8 @@ public class CannonController : MonoBehaviour {
 
 	public void swiftSightScope(float radio) 
 	{
+		moveRate = radio > 0.7f ? 0.2f :
+			radio > 0.3f ? 0.6f : 1f;
 		mainCamera.fieldOfView = 100 - radio * sight_scope_var;
 	}
 }
