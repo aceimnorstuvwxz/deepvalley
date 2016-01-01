4b3d4df6b5d9a11bd16a25d14eec9d9bf6ae8952
Chen Bingfeng <iichenbf@gmail.com>
2016-01-01 19:29:53 +0800

    add ui count-down

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,10 +7,12 @@ public class UIController : MonoBehaviour {
 
 	public Image fenhua_open;
 	public Image fenhua_pos1;
+	public Text textLoadingCount;
 
 	// Use this for initialization
 	void Start () {
 	
+		StartCoroutine ("loadingCount");
 	}
 	
 	// Update is called once per frame
@@ -27,6 +29,17 @@ public class UIController : MonoBehaviour {
 		float alphaOfSecond = radio > 0.5f ? (1f - Mathf.Abs (radio - 1f) / 0.5f) : 0f;
 		fenhua_pos1.color = new Color (1, 1, 1, alphaOfSecond);
 
+	}
+
+
+	IEnumerator loadingCount() {
+		for (int i = 0; i <= 101; i++) {
+			if (i == 101) {
+				textLoadingCount.enabled = false;
+			}
+			textLoadingCount.text = "Loading..."+i.ToString()+"%";
+			yield return null;
+		}
 	}
 
 }
