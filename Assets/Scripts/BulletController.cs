0c84c0a59b5ce167beadfeace07956ac85648d13
Chen Bingfeng <iichenbf@gmail.com>
2016-01-02 19:13:13 +0800

    add bullet

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -5,13 +5,41 @@ public class BulletController : MonoBehaviour {
 
 	public GameObject bullet_prefab;
 
+
+	private GameObject _boreHead;
+	private GameObject _goBullets;
+	private GameObject _goCamera;
+
 	// Use this for initialization
 	void Start () {
-	
+		_boreHead = GameObject.Find ("bore-head");
+		_goBullets = GameObject.Find ("Bullets");
+		_goCamera = GameObject.Find ("main-camera");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void LaunchBullet( )
+	{
+		//position
+
+		Vector3 pos = _boreHead.transform.position;
+
+		GameObject bullet = Instantiate (bullet_prefab) as GameObject;
+		bullet.transform.position = pos;
+//		bullet.transform.rotation =  //TODO diection and speed and collision rigidbody??
+		bullet.transform.SetParent (_goBullets.transform);
+
+
+	}
+
+	public void ShootBatch() {
+
+		// TODO get config of cannon
+		LaunchBullet ();
+
 	}
 }
---
+++ Assets/Scripts/ShootController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootController : MonoBehaviour {
+
+	private BulletController _bulletController;
+
+	// Use this for initialization
+	void Start () {
+
+		_bulletController = GameObject.Find ("cannon").GetComponent<BulletController> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClickShoot() {
+		Debug.Log ("shoot");
+
+		// TODO CD 
+		
+		_bulletController.ShootBatch ();
+
+
+	}
+}
