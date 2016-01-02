ac2568ee1c85c28e4b8aa1aa23ea9b8779260b0d
Chen Bingfeng <iichenbf@gmail.com>
2016-01-02 18:26:19 +0800

    sd

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	public GameObject bullet_prefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
